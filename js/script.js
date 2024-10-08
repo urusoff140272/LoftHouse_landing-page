5bd4086890c38835342a756c10c9f246e4cf4bfe
Andrey Urusov <urusov1402@gmail.com>
2024-10-08 08:12:28 +0300

    feat: add JS

--- js/script.js
+++ js/script.js
@@ -29,3 +29,23 @@ function closeMenu() {
         menuHeader.classList.remove('active');
     }
 }
+
+// -------phone mask---------
+mask('[data-tel-input]');
+
+// Удаляем '+' если больше ничего не введено, чтобы показать placeholder
+const phoneInputs = document.querySelectorAll('[data-tel-input]');
+phoneInputs.forEach((input)=>{
+	input.addEventListener('input', ()=>{
+		if (input.value == '+') {
+            input.value = '';
+        }
+	})
+	input.addEventListener('blur', ()=>{
+		if (input.value == '+') {
+            input.value = '';
+        }
+	})
+});
+
+
